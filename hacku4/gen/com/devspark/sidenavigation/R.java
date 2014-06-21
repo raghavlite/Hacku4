b6000d66ba2ab371e9a972c7a761f7b8c754dfbd
raghavlite <raghavlite@gmail.com>
2014-06-22 05:29:51 +0530

    no new changes but the old style xml file,works with newer api phones

--- hacku4/gen/com/devspark/sidenavigation/R.java
+++ hacku4/gen/com/devspark/sidenavigation/R.java
@@ -32,8 +32,8 @@ public final class R {
 		public static final int side_navigation_width = 0x7f08000f;
 	}
 	public static final class drawable {
-		public static final int side_navigation_shadow_left = 0x7f02007c;
-		public static final int side_navigation_shadow_right = 0x7f02007d;
+		public static final int side_navigation_shadow_left = 0x7f02008f;
+		public static final int side_navigation_shadow_right = 0x7f020090;
 	}
 	public static final class id {
 		public static final int shadow = 0x7f050049;
